5a324cf9b84419d9c3529c101f5be99b728a89ed
Андрей Баранов <30588142+93rus@users.noreply.github.com>
2020-09-17 09:50:13 +0300

    Add files via upload

--- shop/myshop/admin.py
+++ shop/myshop/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Product
 
 @admin.register(Category)
@@ -8,7 +10,14 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'uploaded']
+    list_display = ['name', 'slug', 'image_show', 'price', 'available', 'created', 'uploaded']
     list_filter = ['available', 'created', 'uploaded']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name', )}
+
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe("<img src='{}' width='60' />".format(obj.image.url))
+        return "None"
+
+    image_show.__name__ = "Картинка"
